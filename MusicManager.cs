40329335b9892162e3a81539c2e7ca2d925fc68b
Hiran <gondim.game@gmail.com>
2015-11-30 23:44:25 -0200

    Sistema de controle de audio
    Sistema para facilitar o controle de audio de um programa

---
+++ MusicManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+/// <summary>
+/// Apenas gerencia as alterações e envia para SoundSystem as atualizações da parte de audio.
+/// 
+/// OBS.: precisa de ajustes para caso o programa apenas tenha efeitos sonoros ou apenas musicas.
+/// </summary>
+public class MusicManager : MonoBehaviour {
+
+    SoundSystem test = SoundSystem.Instance;
+    
+    // INIT DECLARAÇÕES DE AUDIO.
+    public List<AudioSource> musica;
+    public List<AudioSource> efeitos;
+
+    public Scrollbar volumeMusica;
+    public Scrollbar volumeEfeitos;
+    // END DECLARAÇÕES DE AUDIO.
+
+    void Start()
+    {
+        //Garante que o Valor inicial sempre vai ser o de SoundSystem caso as barras sejam declaradas.
+        if (volumeMusica != null && volumeEfeitos != null)
+        {
+            volumeMusica.value = test.VolumeMusic;
+            volumeEfeitos.value = test.VolumeEffects;
+        }
+    }
+	void Update ()
+    {
+        //Garante que só vai atualizar o volume de SoundSystem se a classe tiver atribuido as barras de volume.
+        if (volumeMusica != null && volumeEfeitos != null)
+        {
+            test.VolumeMusic = volumeMusica.value;
+            test.VolumeEffects = volumeEfeitos.value;
+
+            test.VolumeControl(musica, test.VolumeMusic);
+            test.VolumeControl(efeitos, test.VolumeEffects);
+        }
+        //Caso não seja declarado as barras de volume ele apenas ganrante que o volume é o mesmo em qualquer canto.
+        else
+        {
+            test.VolumeControl(musica, test.VolumeMusic);
+            test.VolumeControl(efeitos, test.VolumeEffects);
+        }
+    }
+    // INIT METODOS DA CLASSE 
+    public void OnOff() { }
+    // END METODOS DA CLASSE 
+}
---
+++ SoundSystem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+/// <summary>
+/// Garante que tudo envolvendo controle de audio está interligado.
+/// </summary>
+public sealed class SoundSystem: MonoBehaviour
+{
+
+    // INIT ATRIBUTOS
+    private static SoundSystem _AudioConfiguration = new SoundSystem(); //Cria instanci dele mesmo.
+    public static SoundSystem Instance //Propriedade para instanciar a instancia.
+    {
+        get { return _AudioConfiguration; }
+    }
+    static float volumeMusic = 1; //Volume da musica geral.
+    public float VolumeMusic
+    {
+        get { return volumeMusic; }
+        set { volumeMusic = value; }
+    }
+    static float volumeEffects = 1; //Volume dos efeitos gerais.
+    public float VolumeEffects
+    {
+        get { return volumeEffects; }
+        set { volumeEffects = value; }
+    }
+    // END ATRIBUTOS
+
+    // INIT METODOS
+    /// <summary>
+    /// Pausa todas as musicas atribuidas a função.
+    /// </summary>
+    /// <param name="Audio"></param>
+    public void Pause(List<AudioSource> Audio)
+    {
+        for(int i = 0; i < Audio.Count; ++i)
+            Audio[i].Pause();
+    }
+    /// <summary>
+    /// Da play em todas as musicas atribuidas a função.
+    /// </summary>
+    /// <param name="Audio"></param>
+    public void Play(List<AudioSource> Audio)
+    {
+        for (int i = 0; i < Audio.Count; ++i)
+            Audio[i].Play();
+    }
+    /// <summary>
+    /// Garante a mudança de volume das musicas atribuidas a função.
+    /// </summary>
+    /// <param name="Audio"></param>
+    /// <param name="Volume"></param>
+    public void VolumeControl(List<AudioSource> Audio, float Volume)
+    {
+        for (int i = 0; i < Audio.Count; ++i)
+            Audio[i].volume = Volume;
+    }
+    //EMD METODOS
+    private SoundSystem() { }
+    void Update()
+    {
+    }
+}
